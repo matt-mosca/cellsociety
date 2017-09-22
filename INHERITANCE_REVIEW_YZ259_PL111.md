8463041bd6f3f8050b72f4461b9907039d013278
Yiqin Zhou <yz259@duke.edu>
2017-09-22 09:18:09 -0400

    Added lab work for yz259

---
+++ INHERITANCE_REVIEW_YZ259_PL111.md
@@ -0,0 +1,6 @@
+* My design is hiding the instance variables and how the logic of each simulation from the front end. 
+* I am intend to build a cell super class and simulation super class.
+* Super classes are open so that people can add more sub classes to it but every sub class is closed to ensure no damage is made.
+* We have separated the super class and sub class, which make the hierarchies and structures easy to modify.
+* My area is linked to front end through 2D array.
+* These dependencies cannot be avoided due to the nature of interaction between front end and back end.  
--- src/cellsociety_team05/SimulationSegregation.java
+++ src/cellsociety_team05/SimulationSegregation.java
@@ -1,5 +1,73 @@
 package cellsociety_team05;
 
+import java.util.Random;
+
 public class SimulationSegregation {
+	private CellSociety[][] array;      
+	private double satisfactionPercentage;
+	private int numberOfCells;
+	private double emptyPercentage;
+	private double redToBlueRatio;
+	
+	//0 is empty, 1 is red, 2 is blue
+	
+	public SimulationSegregation(int cellNumberHorizontal, int cellNumberVertical, double emptyPercentage, 
+			double satisfactionPercentage, double redToBlueRatio) {
+		//set up instance variables, put 0s in every cell
+		array=new CellSociety [cellNumberHorizontal][cellNumberVertical];
+		this.emptyPercentage=emptyPercentage;
+		this.satisfactionPercentage=satisfactionPercentage;
+		this.redToBlueRatio=redToBlueRatio;
+		this.numberOfCells=cellNumberHorizontal*cellNumberVertical;
+		
+		
+	}
+	
+	public void initializeScene() {
+		//according to percentage, do random function
+		//call cell to change type 
+		int redNumber=findNumber(1);
+		int blueNumber=findNumber(2);
+		Random rand=new Random();
+		
+		
+		
+
+		
+	}
+	
+	public int findNumber(int state) {
+		int empty=(int) (numberOfCells*emptyPercentage);
+		int filled=numberOfCells-empty;
+		if (state==0) {
+			return empty;				
+		}
+		if (state==1) {
+			return (int) (filled/(redToBlueRatio+1)*redToBlueRatio);	 
+		}
+		if (state==2) {
+			return (int) (filled/(redToBlueRatio+1));
+		}
+		return 0;
+	}
+	
+	public void update() {
+		//set up a loop, go through every cell
+		//call whetherSatisfied
+		
+	}
+	
+	
+
+	public CellSociety[][] getArray() {
+		return array;
+	}
+
+	public void setArray(
+			CellSociety[][] array) {
+		this.array = array;
+	}
+	
+	
 
 }
