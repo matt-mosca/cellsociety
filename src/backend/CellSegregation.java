2df3066398f20fda9d90a4f5f5df17cf6687d3a8
MATTHEW MOSCA <MJM141@duke.edu>
2017-09-23 17:08:23 -0400

    CellSegregation.java superclass methods removed

--- src/backend/CellSegregation.java
+++ src/backend/CellSegregation.java
@@ -7,54 +7,17 @@ import javafx.scene.image.ImageView;
 
 public class CellSegregation extends Cell{
 	
-
-
 	public CellSegregation(int state, Image image, ArrayList<Cell> neighborCells, int rowNumber, int columnNumber) {
 		super(state, image, neighborCells, rowNumber, columnNumber);
 		// TODO Auto-generated constructor stub
-
-	}
-	public int getState() {
-		return super.getState();
-	}
-	public void changeState(int state) {
-		super.changeState(state);
-	}
-	public Image getImage() {
-		return super.getImage();
-	}
-	public void setImage(
-			Image image) {
-		super.setImage(image);
 	}
 	
-	public ArrayList<Cell> getNeighborCells() {
-		return super.getNeighborCells();
+	//For testing
+	public static void main(String[] args) {
+//		Image image = new Image(getClass().getClassLoader().getResourceAsStream(imageName));
+		Image image = new Image("the");
+		ArrayList<Cell> neighbors = new ArrayList<Cell>();
+		CellSegregation test = new CellSegregation(0, image, neighbors, 1, 1);
+		test.changeState(2);
 	}
-	public void setNeighborCells(
-			ArrayList<Cell> neighborCells) {
-		super.setNeighborCells(neighborCells);
-	}
-
-	public int getRowNumber() {
-		return super.getRowNumber();
-	}
-
-	public void setRowNumber(
-			int rowNumber) {
-		super.setRowNumber(rowNumber);
-	}
-
-	public int getColumnNumber() {
-		return super.getColumnNumber();
-	}
-
-	public void setColumnNumber(
-			int columnNumber) {
-		super.setColumnNumber(columnNumber);
-	}
-
-
-	
-
 }
