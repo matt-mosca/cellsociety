c9dea8a017710bc0e92e2e3adf0ecef0c7d6b042
Yiqin Zhou <yz259@duke.edu>
2017-09-24 10:26:38 -0400

    Finished segregation xml read

--- src/frontend/UserInput.java
+++ src/frontend/UserInput.java
@@ -3,6 +3,50 @@ package frontend;
 /*
  * Author: Venkat S.
  */
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
 public class UserInput {
+	
+
+
+
+	  public void getSegregation() {
+
+	    try {
+
+		File fXmlFile = new File("Segregation.xml");
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(fXmlFile);
+
+		doc.getDocumentElement().normalize();
+
+		String title = doc.getElementsByTagName("name").item(0).getTextContent();
+		String satisfactionPercentage = doc.getElementsByTagName("satisfactionPercentage").item(0).getTextContent();
+		String cellNumberHorizontal = doc.getElementsByTagName("cellNumberHorizontal").item(0).getTextContent();
+		String cellNumberVertical = doc.getElementsByTagName("cellNumberVertical").item(0).getTextContent();
+		String emptyPercentage = doc.getElementsByTagName("emptyPercentage").item(0).getTextContent();
+		String redToBlueRatio = doc.getElementsByTagName("redToBlueRatio").item(0).getTextContent();
+		System.out.println(title);
+		System.out.println(satisfactionPercentage);
+		System.out.println(cellNumberHorizontal);
+		System.out.println(cellNumberVertical);
+		
+
+		
+		
+	    } catch (Exception e) {
+		e.printStackTrace();
+	    }
+	  }
+
+	
 
 }
