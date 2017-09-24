403a3a17c94f1dd5685b6e9b0a2224801130cf44
Venkat Subramaniam <venkat.subramaniam@duke.edu>
2017-09-24 13:11:46 -0400

    Just changed up the UserInput file to not have globals

--- src/frontend/UserInput.java
+++ src/frontend/UserInput.java
@@ -1,7 +1,7 @@
 package frontend;
 
 /*
- * Author: Venkat S.
+ * Author: Venkat S., Yiqin Zhou
  */
 
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -13,16 +13,20 @@ import java.io.File;
 import java.io.IOException;
 
 public class UserInput {
-	  String title;
-	  String cellNumberHorizontal;
-	  String cellNumberVertical;
-	  String emptyPercentage;
-	  String redToBlueRatio;
-	  String satisfactionPercentage;
-	  String minBreedDaysForSharks;
-	  String minBreedDaysForFish;
-	  String maxStarveDaysForSharks;
-	  String probCatch;
+//	private static final int NORMAL_ARRAY_LENGTH = 5;
+//	private static final int WATOR_ARRAY_LENGTH = 8;
+//	private static final int SEG_ARRAY_LENGTH = 6;
+//	  private String title;
+//	  private String cellNumberHorizontal;
+//	  private String cellNumberVertical;
+//	  private String emptyPercentage;
+//	  private String redToBlueRatio;
+//	  private String satisfactionPercentage;
+//	  private String minBreedDaysForSharks;
+//	  private String minBreedDaysForFish;
+//	  private String maxStarveDaysForSharks;
+//	  private String probCatch;
+	  private String[] finalParams;
 	  
 	  public UserInput() {
 		  
@@ -38,8 +42,12 @@ public class UserInput {
 	    
 		Document doc = getFile("Segregation.xml");
 		doc.getDocumentElement().normalize();	
-		satisfactionPercentage = doc.getElementsByTagName("satisfactionPercentage").item(0).getTextContent();
-		getInitialSetUp(doc);
+		String satisfactionPercentage = doc.getElementsByTagName("satisfactionPercentage").item(0).getTextContent();
+//		finalParams = new String[SEG_ARRAY_LENGTH];
+		String[] basic = new String[1];
+		basic[0] = satisfactionPercentage;
+		getInitialSetUp(doc, basic);
+//		makeFinalArray(fun);
 		
 			
 		
@@ -57,10 +65,16 @@ public class UserInput {
 	    
 		Document doc = getFile("WaTor.xml");
 		doc.getDocumentElement().normalize();	
-		minBreedDaysForSharks = doc.getElementsByTagName("minBreedDaysForSharks").item(0).getTextContent();
-		minBreedDaysForFish = doc.getElementsByTagName("minBreedDaysForFish").item(0).getTextContent();
-		maxStarveDaysForSharks = doc.getElementsByTagName("maxStarveDaysForSharks").item(0).getTextContent();
-		getInitialSetUp(doc);
+		String minBreedDaysForSharks = doc.getElementsByTagName("minBreedDaysForSharks").item(0).getTextContent();
+		String minBreedDaysForFish = doc.getElementsByTagName("minBreedDaysForFish").item(0).getTextContent();
+		String maxStarveDaysForSharks = doc.getElementsByTagName("maxStarveDaysForSharks").item(0).getTextContent();
+//		finalParams = new String[WATOR_ARRAY_LENGTH];
+		String[] basic = new String[3];
+		basic[0] = minBreedDaysForSharks;
+		basic[1] = minBreedDaysForFish;
+		basic[2] = maxStarveDaysForSharks;
+		getInitialSetUp(doc, basic);
+//		makeFinalArray();
 		
 	
 		
@@ -70,15 +84,24 @@ public class UserInput {
 		
 	  }
 	  
-	  public void getFire() {
+	
+
+
+
+	public void getFire() {
 			
 			
 		    try {
 		    
 			Document doc = getFile("Fire.xml");
 			doc.getDocumentElement().normalize();	
-		    probCatch = doc.getElementsByTagName("probCatch").item(0).getTextContent();
-			getInitialSetUp(doc);
+		    String probCatch = doc.getElementsByTagName("probCatch").item(0).getTextContent();
+//		    finalParams = new String[SEG_ARRAY_LENGTH];
+		    String[] basic = new String[0];
+		    basic[0] = probCatch;
+			getInitialSetUp(doc, basic);
+//			makeFinalArray(fun);
+			
 			
 				
 			
@@ -95,7 +118,8 @@ public class UserInput {
 		    
 			Document doc = getFile("GameOfLife.xml");
 			doc.getDocumentElement().normalize();	
-			getInitialSetUp(doc);
+			getInitialSetUp(doc, null);
+//			makeFinalArray(fun);
 			
 				
 			
@@ -108,9 +132,7 @@ public class UserInput {
 
 
 
-	public Document getFile(String fileName)
-			throws ParserConfigurationException,
-			SAXException, IOException {
+	public Document getFile(String fileName) throws ParserConfigurationException, SAXException, IOException {
 		File fXmlFile = new File(fileName);
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -121,15 +143,37 @@ public class UserInput {
 
 
 
-	public void getInitialSetUp(
-			Document doc) {
-		title = doc.getElementsByTagName("name").item(0).getTextContent();
-		cellNumberHorizontal = doc.getElementsByTagName("cellNumberHorizontal").item(0).getTextContent();
-		cellNumberVertical = doc.getElementsByTagName("cellNumberVertical").item(0).getTextContent();
-		emptyPercentage = doc.getElementsByTagName("emptyPercentage").item(0).getTextContent();
-		redToBlueRatio = doc.getElementsByTagName("redToBlueRatio").item(0).getTextContent();
+	public void getInitialSetUp(Document doc, String[] second) {
+		String[] things = new String[5];
+		String title = doc.getElementsByTagName("name").item(0).getTextContent();
+		String cellNumberHorizontal = doc.getElementsByTagName("cellNumberHorizontal").item(0).getTextContent();
+		String cellNumberVertical = doc.getElementsByTagName("cellNumberVertical").item(0).getTextContent();
+		String emptyPercentage = doc.getElementsByTagName("emptyPercentage").item(0).getTextContent();
+		String redToBlueRatio = doc.getElementsByTagName("redToBlueRatio").item(0).getTextContent();
+		things[0] = title;
+		things[1] = cellNumberHorizontal;
+		things[2] = cellNumberVertical;
+		things[3] = emptyPercentage;
+		things[4] = redToBlueRatio;
+		makeFinalArray(things, second);
+//		return things;
 	}
-
+	
+	  private void makeFinalArray(String[] fun, String[] second) {
+		   if (second==null) {
+			   finalParams = fun;
+			   return;
+		   }
+		   String[] thing = new String[fun.length+second.length];
+		   for (int i=0; i<fun.length; i++) {
+			  thing[i] = fun[i];
+		   }
+		   for (int i=fun.length; i<finalParams.length; i++) {
+			   thing[i]= second[i];
+		   }
+		   this.finalParams = thing;
+		  }
+			
 	
 
 }
