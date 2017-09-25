6992fc5765debd5a4f50024c0a41358978a58202
Venkat Subramaniam <venkat.subramaniam@duke.edu>
2017-09-25 03:59:52 -0400

    just committing before sleeping tonight

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,12 @@
  * Author: Venkat
  */
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Scanner;
-
 import frontend.SimDisplay;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.paint.Paint;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.util.Duration;
+
 
 public class Main extends Application{
 	private static final int SIZEx = 700;
