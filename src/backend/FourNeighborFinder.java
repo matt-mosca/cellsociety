e75759e456f881e782f092b8390268264511277d
MATTHEW MOSCA <MJM141@duke.edu>
2017-09-29 02:08:26 -0400

    FourNeighborFinder.java added

---
+++ src/backend/FourNeighborFinder.java
@@ -0,0 +1,27 @@
+package backend;
+
+import java.util.ArrayList;
+
+public class FourNeighborFinder extends NeighborFinder {
+	public FourNeighborFinder(Cell[][] cells, int xPos, int yPos) {
+		super(cells, xPos, yPos);
+	}
+	
+	public ArrayList<Cell> findNeighbors() {
+		Cell[][] cellArray = getMyCellArray();
+		int xPos = getMyXPosition();
+		int yPos = getMyYPosition();
+		int cellNumberVertical = cellArray.length;
+		int cellNumberHorizontal = cellArray[0].length;
+		ArrayList<Cell> neighbors = new ArrayList<Cell>();
+		if (yPos - 1 >= 0)
+			neighbors.add(cellArray[yPos - 1][xPos]);
+		if (xPos-1>=0)
+			neighbors.add(cellArray[yPos][xPos - 1]);
+		if (xPos+1<=cellNumberHorizontal - 1)
+			neighbors.add(cellArray[yPos][xPos + 1]);
+		if (yPos+1<=cellNumberVertical - 1)
+			neighbors.add(cellArray[yPos + 1][xPos]);
+		return neighbors;
+	}
+}
