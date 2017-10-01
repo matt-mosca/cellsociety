878a19b318821cf7637072dcf21ea3d9689dc187
MATTHEW MOSCA <MJM141@duke.edu>
2017-10-01 19:13:10 -0400

    CellRPS.java for Rock, Paper, Scissors (RPS) simulation added

---
+++ src/backend/CellRPS.java
@@ -0,0 +1,33 @@
+package backend;
+
+import java.util.List;
+import javafx.scene.paint.Color;
+
+public class CellRPS extends Cell{
+	public static final int EMPTY = 0;
+	public static final int RED = 1;
+	public static final int BLUE = 2;
+	public static final int GREEN = 3;
+	
+	//0 is darkest, 9 is lightest
+	private int gradientLevel;
+	
+	public CellRPS(int state, Color color, List<Cell> neighborCells, int rowNumber, int columnNumber, int gradientLevel) {
+		super(state, color, neighborCells, rowNumber, columnNumber);
+		this.gradientLevel = gradientLevel;
+	}
+	
+	public int getGradientLevel() {
+		return gradientLevel;
+	}
+	
+	public void upgrade() {
+		if(gradientLevel != 0)
+			gradientLevel--;
+	}
+	
+	public void downgrade() {
+		if(gradientLevel < 9)
+			gradientLevel++;
+	}
+}
